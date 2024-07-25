64483a4d22896aa21773d93293bce593a8635a8b
sleek-ennju <ihenachochukwudi@gmail.com>
2024-07-25 09:58:15 +0100

    adjusted dark mode on the task component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -23,16 +23,16 @@ const Task = ({id, content, state, todoAction}: task) => {
     }
 
   return (
-    <div className="flex gap-2 sm:gap-3 md:gap-4 p-4 bg-base-500 dark:bg-white border border-base-400 rounded-md">
+    <div className="flex gap-2 sm:gap-3 md:gap-4 p-4 dark:bg-base-500 bg-white border dark:border-base-400 border-blue-dark rounded-md">
         <label htmlFor={id} className="group self-start cursor-pointer">
             <input checked={state} onChange={handleChange} type="checkbox" id={id} hidden />
             {
                 state ? ( <CheckTrue /> ) : ( <CheckFalse /> )
             }
         </label>
-        <p className={classNames(state ? "line-through text-base-300 dark:text-base-500" : "text-base-100 dark:text-base-700", "flex-1 text-sm md:text-base transition-all duration-300")}>{content}</p>
+        <p className={classNames(state ? "line-through dark:text-base-300 text-base-300" : "dark:text-base-100 text-base-400", "flex-1 text-sm md:text-base transition-all duration-300")}>{content}</p>
         <button onClick={handleDelete} className="self-start group">
-            <BsTrash3Fill className="size-4 md:size-5 text-base-100 dark:text-base-700 group-hover:text-danger transition-all duration-300" />
+            <BsTrash3Fill className="size-4 md:size-5 dark:text-base-100 text-base-400 group-hover:text-danger transition-all duration-300" />
         </button>
     </div>
   )
